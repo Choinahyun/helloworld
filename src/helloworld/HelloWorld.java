9a4cdc8be4bb38dead1af956389f20cc03b91e00
T3AD20 <T3AD20@G209-27>
2017-01-02 15:45:00 +0900

    commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+/*
+ * Hello World 예제
+*/
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String s = "Hello World";
+		// Hello World 출력
+		System.out.println(s);  
+
+	}
+
+}
